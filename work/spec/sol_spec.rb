9e91e4d2d75d91e4fc268f412b840b1248a6b141
Arthur Reutenauer <arthur@reutenauer.eu>
2017-10-22 01:42:39 +0200

    Some more.

--- work/spec/sol_spec.rb
+++ work/spec/sol_spec.rb
@@ -9,7 +9,9 @@ describe Sol do
       out = sol.process('Husby sn, tätort, Dalarna')
       expect(out).to eq '<head><placeName>Husby</placeName></head> <P><locale>sn</locale>, <locale>tätort</locale>, <location><region type="landskap">Dalarna</region></location>'
       out = sol.process('Hurva sn, tätort, Frosta hd, Skåne')
-      expect(out).to eq '<head><placeName>Hurva</placeName></head> <P><locale>sn</locale>, <locale>tätort</locale>, <location><district type="härad">Frosta hd</distrit>, <region type="landskap">Skåne</region></location>'
+      expect(out).to eq '<head><placeName>Hurva</placeName></head> <P><locale>sn</locale>, <locale>tätort</locale>, <location><district type="härad">Frosta hd</district>, <region type="landskap">Skåne</region></location>'
+      out = sol.process('Vätteryd torp, gravfält, Norra Mellby sn, Västra Göinge hd, Skåne')
+      expect(out).to eq '<head><placeName>Vätteryd</placeName></head> <P><locale>torp</locale>, <locale>gravfält</locale>, <district type="socken">Norra Mellby sn</district>, <district type="härad">Västra Göinge hd</district>, <region type="landskap">Skåne</region>'
     end
   end
 end
