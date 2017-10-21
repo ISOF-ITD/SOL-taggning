5213f3d933299cff2658ccc1ee583d45300ca8eb
Arthur Reutenauer <arthur@reutenauer.eu>
2017-10-22 01:16:27 +0200

    This one is actually not going to be that easy, I think.

--- work/lib/sol.rb
+++ work/lib/sol.rb
@@ -1,2 +1,4 @@
 class Sol
+  def process(line)
+  end
 end
--- work/spec/sol_spec.rb
+++ work/spec/sol_spec.rb
@@ -3,9 +3,11 @@ require 'spec_helper'
 describe Sol do
   let(:sol) { Sol.new }
 
+  # TODO Hard examples Bålsta kn
   describe '#process' do
     it "processes a few entries" do
       out = sol.process('Husby sn, tätort, Dalarna')
+      expect(out).to eq '<head><placeName>Husby</placeName> <P><locale>sn</locale>, <locale>tätort</locale>, <region type="landskap">Dalarna</region>'
     end
   end
 end
