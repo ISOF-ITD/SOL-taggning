e8b7263d20ba45b3053c196e7a2000e4f737a99d
Arthur Reutenauer <arthur@reutenauer.eu>
2017-10-22 18:14:19 +0200

    Cleaned up.

--- work/lib/sol.rb
+++ work/lib/sol.rb
@@ -13,34 +13,30 @@ class Sol
     end
     length = array.count
     array.each_with_index do |element, index|
-      if element == 'tätort'
-        xml_strip += '<locale>tätort</locale>, ' # FIXME Find all tätorter that are not locales
+      element =~ /^(.*) (.*)$/
+      locale = $2
+      case locale
+      when 'sn'
+        tag = 'district'
+        attr = 'socken'
+      when 'hd'
+        tag = 'district'
+        attr = 'härad'
+      end
+
+      if index == length - 1
+        tag = 'region'
+        attr = 'landskap'
+      end
+
+      # byebug
+
+      xml_strip += "<#{tag} type=\"#{attr}\">#{element}</#{tag}>"
+
+      if index == length - 1
+        xml_strip += '</location>'
       else
-        element =~ /^(.*) (.*)$/
-        locale = $2
-        case locale
-        when 'sn'
-          tag = 'district'
-          attr = 'socken'
-        when 'hd'
-          tag = 'district'
-          attr = 'härad'
-        end
-
-        if index == length - 1
-          tag = 'region'
-          attr = 'landskap'
-        end
-
-        # byebug
-
-        xml_strip += "<#{tag} type=\"#{attr}\">#{element}</#{tag}>"
-
-        if index == length - 1
-          xml_strip += '</location>'
-        else
-          xml_strip += ', '
-        end
+        xml_strip += ', '
       end
     end
 
