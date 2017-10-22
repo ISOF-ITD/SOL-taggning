dbefb3bbd5194ef4f429ee2a864997fe313f0c4c
Arthur Reutenauer <arthur@reutenauer.eu>
2017-10-22 17:38:44 +0200

    Three more specs, and some notes!

--- work/spec/sol_spec.rb
+++ work/spec/sol_spec.rb
@@ -30,5 +30,23 @@ describe Sol do
       expect(out).to eq '<head><placeName>Västrum</placeName></head> <P><locale>sn</locale>, <location><district type="härad">Södra Tjusts hd</district>, <region type="landskap">Småland</region></location>'
       # FIXME Add forms with ‘och’, see e. g. pp. 176-177.  No plural abbrevation for härad?
     end
+
+    it "handles the case of two härad" do
+      out = sol.process('Kinnekulle berg, Kinne och Kinnefjärdings hd, Västergötland')
+      expect(out).to eq '<head><placeName>Kinnekulle</placeName></head> <P><locale>berg</locale>, <location><district type="socken">Kinne</district> och </district type="socken">Kinnefjärdings</district> snr, <region type="landsksap">Västergötland</region></location>'
+    end
+
+    it "handles the case of two socknar" do
+      out = sol.process('Kivik tätort, Södra Mellby och Vitaby snr, Albo hd, Skåne')
+      expect(out).to eq '<head><placeName>Kivik</placeName></head> <P><locale>tätort</locale>, <location><district type="socken">Södra Mellby</district> och <district type="socken">Vitaby</district> snr, <district type="härad">Albo hd</district>, <region type="landskap">Skåne</region></location>'
+    end
+
+    it "for real!" do
+      out = sol.process('Klagshamn samhälle, Västra Klagstorps och Tygelsjö snr, Oxie hd, Skåne')
+      expect(out).to eq '<head><placeName>Klagshamn</placeName></head> <P><locale>samhälle</locale>, <location><district type="socken">Västra Klagstorps</district> och <district type="socken">Tygelsjö</district> snr, <district type="härad">Oxie hd</district>, <region type="landskap">Skåne</region></location>'
+    end
   end
+
+  # TODO Handle place name elements, f.d., longer strings such as kavsområde
+  # avgränsat av Jylland och Själland samt svenska västkusten (Kattegatt)
 end
