ad5be5cf07a2b3ff9e0dfef93d387bf1978418c0
Arthur Reutenauer <arthur@reutenauer.eu>
2017-10-22 14:50:23 +0200

    Fixed some things.

--- work/spec/sol_spec.rb
+++ work/spec/sol_spec.rb
@@ -15,19 +15,19 @@ describe Sol do
       expect(out).to eq '<head><placeName>Hurva</placeName></head> <P><locale>sn</locale>, <locale>tätort</locale>, <location><district type="härad">Frosta hd</district>, <region type="landskap">Skåne</region></location>'
     end
 
-    it "processes a place with an extra locale" do
+    it "processes a point with an extra locale" do
       out = sol.process('Vätteryd torp, gravfält, Norra Mellby sn, Västra Göinge hd, Skåne')
-      expect(out).to eq '<head><placeName>Vätteryd</placeName></head> <P><locale>torp</locale>, <locale>gravfält</locale>, <district type="socken">Norra Mellby sn</district>, <district type="härad">Västra Göinge hd</district>, <region type="landskap">Skåne</region>'
+      expect(out).to eq '<head><placeName>Vätteryd</placeName></head> <P><locale>torp</locale>, <locale>gravfält</locale>, <location><district type="socken">Norra Mellby sn</district>, <district type="härad">Västra Göinge hd</district>, <region type="landskap">Skåne</region></location>'
     end
 
     it "processes a parish with a compound name" do
       out = sol.process('Västra Vram sn, Gärds hd, Skåne')
-      expect(out).to eq '<head><placeName>Västra Vram</placeName></head> <P><locale>sn</locale>, <location><district type="härad">Gärds härad</district>, <region type="landskap">Skåne</region>'
+      expect(out).to eq '<head><placeName>Västra Vram</placeName></head> <P><locale>sn</locale>, <location><district type="härad">Gärds härad</district>, <region type="landskap">Skåne</region></location>'
     end
 
     it "processes a simple parish" do
       out = sol.process('Västrum sn, Södra Tjusts hd, Småland')
-      expect(out).to eq '<head><placeName>Västrum</placeName></head> <P><locale>sn</locale>, <location><district type="härad">Södra Tjusts hd</district>, <region type="landskap">Småland</region>'
+      expect(out).to eq '<head><placeName>Västrum</placeName></head> <P><locale>sn</locale>, <location><district type="härad">Södra Tjusts hd</district>, <region type="landskap">Småland</region></location>'
       # FIXME Add forms with ‘och’, see e. g. pp. 176-177.  No plural abbrevation for härad?
     end
   end
