ee6317a0de65da080085ae4d0befa4056947ddb7
Arthur Reutenauer <arthur@reutenauer.eu>
2017-11-06 23:48:37 +0100

    Forgot that!

--- work/spec/spec_helper.rb
+++ work/spec/spec_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 require 'solig'
 
 def loadparagraph(id)
-  featdir = File.expand_path('../features/w:p', __FILE__)
+  featdir = File.expand_path('../fixtures/w:p', __FILE__)
   docstring = '<w:document xmlns:w="http://schemas.openxmlformats.org/2006/wordprocessml/2006/main" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml">' +
     File.read(File.join(featdir, id + '.xml')) +
     '</w:document>'
