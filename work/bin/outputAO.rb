4d8b9b9e5f3b3e7d7aaf6dcb210f8ea7c746838e
Arthur Reutenauer <arthur@reutenauer.eu>
2017-11-11 23:50:46 +0100

    Updated output file name.

--- work/bin/outputAO.rb
+++ work/bin/outputAO.rb
@@ -5,7 +5,7 @@ require_relative '../lib/solig'
 solig = Solig.new
 sol2 = REXML::Document.new File.read File.expand_path '../../SOL2-from-docx-with-edits.xml', __FILE__
 paragrafer = sol2.root.elements.first.elements
-pars = File.open(File.expand_path('../../pars.xml', __FILE__), 'w')
+pars = File.open(File.expand_path('../../artiklarA-O.xml', __FILE__), 'w')
 (444..4299).each do |paragraf|
   pars.puts solig.unword paragrafer[paragraf]
   pars.puts ''
