307f676f803ff907c46ec5421b20c937a7e0023b
Arthur Reutenauer <arthur@reutenauer.eu>
2017-11-13 14:26:21 +0100

    This has better chances of working.

--- work/bin/outputAO.rb
+++ work/bin/outputAO.rb
@@ -6,7 +6,7 @@ solig = Solig.new
 sol2 = REXML::Document.new File.read File.expand_path '../../SOL2-from-docx-with-edits.xml', __FILE__
 paragrafer = sol2.root.elements.first
 
-pars = solig.process_range (444..4299)
+pars = solig.process_range paragrafer, (444..4299)
 
 pars_file = File.open(File.expand_path('../../artiklarA-O.xml', __FILE__), 'w')
 pars_file.puts pars.to_s
