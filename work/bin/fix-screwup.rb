5f4e8c593d956855293e02eae011a43f229e96a7
Arthur Reutenauer <arthur@reutenauer.eu>
2017-11-20 17:12:46 +0100

    Ha!

--- work/bin/fix-screwup.rb
+++ work/bin/fix-screwup.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/solig'
-sol2_docx = Document.new File.read 'SOL2-from-docx-with-edits.xml'
+# sol2_docx = Document.new File.read 'SOL2-from-docx-with-edits.xml'
 sol2_tei = Document.new File.read 'SOL2.xml'
 
 File.read('list-of-screwups.txt').each_line do |id|
+  id.strip!
   article_tei = XPath.first(sol2_tei, "//div[@xml:id='#{id}']")
+  puts title
   title = XPath.first(article_tei, 'head/placeName').title
   byebug
 end
