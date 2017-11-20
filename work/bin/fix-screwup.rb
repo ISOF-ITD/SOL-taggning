c39db1f0ecf7fc6243c0cc323197cf57898fa704
Arthur Reutenauer <arthur@reutenauer.eu>
2017-11-20 17:06:21 +0100

    Changed mode to make script executable.

--- work/bin/fix-screwup.rb
+++ work/bin/fix-screwup.rb
@@ -7,5 +7,5 @@ sol2_tei = Document.new File.read 'SOL2.xml'
 File.read('list-of-screwups.txt').each_line do |id|
   article_tei = XPath.first(sol2_docx, "//div[@xml:id='#{id}']")
   title = XPath.first(article_tei, 'head/placeName').title
-  article_docx = XPath.first(sol2_docx, '//div[@
+  byebug
 end
