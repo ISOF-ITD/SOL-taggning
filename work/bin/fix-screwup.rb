9eb3a4a591c3d2ac66fc95777e8fd03bebe661b2
Arthur Reutenauer <arthur@reutenauer.eu>
2017-11-20 17:05:36 +0100

    Shall we try something else?

--- work/bin/fix-screwup.rb
+++ work/bin/fix-screwup.rb
@@ -5,5 +5,7 @@ sol2_docx = Document.new File.read 'SOL2-from-docx-with-edits.xml'
 sol2_tei = Document.new File.read 'SOL2.xml'
 
 File.read('list-of-screwups.txt').each_line do |id|
-  article_docx
+  article_tei = XPath.first(sol2_docx, "//div[@xml:id='#{id}']")
+  title = XPath.first(article_tei, 'head/placeName').title
+  article_docx = XPath.first(sol2_docx, '//div[@
 end
