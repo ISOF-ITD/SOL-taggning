8f018c6abf2804252cdfd05b9cf6f5aca5065b78
Arthur Reutenauer <arthur@reutenauer.eu>
2017-11-20 17:03:14 +0100

    Inte docx2!

---
+++ work/bin/fix-screwup.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/solig'
+sol2_docx2 = Document.new File.read 'SOL2-from-docx-with-edits.xml'
+sol2_tei = Document.new File.read 'SOL2.xml'
+
+File.read('list-of-screwups.txt').each_line do |id|
+  article_docx
+end
