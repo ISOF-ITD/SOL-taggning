6672efa6ba4f2ea82696b0bf2f7110371da6ccf6
Arthur Reutenauer <arthur@reutenauer.eu>
2017-11-20 17:16:01 +0100

    Let’s run all of it now ...

--- work/bin/fix-screwup.rb
+++ work/bin/fix-screwup.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/solig'
-# sol2_docx = Document.new File.read 'SOL2-from-docx-with-edits.xml'
+sol2_docx = Document.new File.read 'SOL2-from-docx-with-edits.xml'
 sol2_tei = Document.new File.read 'SOL2.xml'
 
 File.read('list-of-screwups.txt').each_line do |id|
