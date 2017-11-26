cdccaafe2bd9b6dfe1e36964b92ea17cf36a7094
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2017-11-26 23:34:39 +0000

    Preparing for some more future workgit add spec/solig_spec.rb

--- work/spec/solig_spec.rb
+++ work/spec/solig_spec.rb
@@ -1133,6 +1133,7 @@ describe Solig do
       expect { solig.analyse_kursiv(bjärnum) }.not_to raise_exception
     end
 
+    it "sees other occurrences of belägg after the dash, such as (<kurs>bel.</kurs>) with a date nearby"
     it "adds the date to the mix"
     it "extracts the language name"
   end
