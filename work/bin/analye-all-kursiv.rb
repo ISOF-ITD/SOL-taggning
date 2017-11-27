41bf72e2f0eab3e8bda376de0209ca0aa24e67b8
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2017-11-27 00:35:10 +0000

    Let’s deal with it that way.

--- work/bin/analye-all-kursiv.rb
+++ work/bin/analye-all-kursiv.rb
@@ -3,8 +3,18 @@ require_relative '../lib/solig'
 solig = Solig.new
 sol2 = Document.new File.read File.expand_path '../../SOL2.xml', __FILE__
 
+problems = File.open 'problems.txt', 'w'
+
 (3..5919).each do |n|
   div = XPath.first(sol2, "/TEI/text/body/div[#{n}]")
   puts "Analysing #{XPath.first(div, 'head/placeName')}"
-  solig.analyse_kursiv(div)
+  begin
+    solig.analyse_kursiv(div)
+  rescue
+    id = div.attributes['xml:id']
+    puts "Problem!  xml:id == #{id}"
+    problems.puts id
+  end
 end
+
+problems.close
