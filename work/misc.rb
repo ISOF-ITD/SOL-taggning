7ec09aabedf0f806af72b8efedc22cffccd6552f
Arthur Reutenauer <arthur@reutenauer.eu>
2018-03-23 09:14:41 +0100

    Major cleanup.

--- work/misc.rb
+++
@@ -1,3 +0,0 @@
-places = []
-sol2.each_element('/TEI/text/body/div/p/span[@type="locale"]') { |locale| if locale.text == 'sn' then placenode = XPath.first(locale.parent.parent, 'head/placeName'); if placenode then place = placenode.text; puts place; places << place end end }
-
